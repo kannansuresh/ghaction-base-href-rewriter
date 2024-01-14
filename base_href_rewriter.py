8e520874857c61175d43727abcde1c52d06e9700
Kannan Suresh <kannansuresh@outlook.com>
2024-01-14 10:03:25 +0530

    Refactor base_href_rewriter.py to support GitHub Actions environment variables

--- base_href_rewriter.py
+++ base_href_rewriter.py
@@ -1,33 +1,48 @@
 import os
 import re
+import sys
 import glob
 import argparse
-import sys
+import asyncio
 
 
 def get_args():
-    parser = argparse.ArgumentParser(
-        description='Rewrite base href in HTML files')
-    parser.add_argument('--base_href',
-                        help='The new base href value')
-    parser.add_argument('--html_path', help='Path to the HTML file to rewrite')
-    parser.add_argument(
-        '--html_glob', help='Glob pattern to match multiple HTML files')
+    if os.getenv('GITHUB_ACTIONS'):
+        base_href = os.getenv('INPUT_BASE_HREF')
+        html_path = os.getenv('INPUT_HTML_PATH')
+        html_glob = os.getenv('INPUT_HTML_GLOB')
+    else:
+        parser = argparse.ArgumentParser(
+            description='Rewrite base href in HTML files')
+        parser.add_argument('--base_href', required=True,
+                            help='The new base href value')
+        parser.add_argument(
+            '--html_path', help='Path to the HTML file to rewrite')
+        parser.add_argument(
+            '--html_glob', help='Glob pattern to match multiple HTML files')
 
-    args = parser.parse_args()
+        args = parser.parse_args()
 
-    base_href = os.getenv('INPUT_BASE_HREF', args.base_href)
-    html_path = os.getenv('INPUT_HTML_PATH', args.html_path)
-    html_glob = os.getenv('INPUT_HTML_GLOB', args.html_glob)
+        base_href = args.base_href
+        html_path = args.html_path
+        html_glob = args.html_glob
 
     return base_href, html_path, html_glob
 
 
 async def run_rewriter(base_href, html_path, html_glob):
     try:
+        prefix = '--'
+        if os.getenv('GITHUB_ACTIONS'):
+            prefix = ''
+
+        if not (base_href):
+            raise ValueError(
+                f'{prefix}base_href must be set')
+
         if not (html_path or html_glob):
             raise ValueError(
-                'At least one of --html_path or --html_glob must be set')
+                f'At least one of {prefix}html_path or {prefix}html_glob must be set')
 
         if html_path:
             rewrite_file(html_path, base_href)
@@ -65,7 +80,6 @@ def rewrite_file(file_path, base_href):
 
 def main():
     base_href, html_path, html_glob = get_args()
-    import asyncio
     asyncio.run(run_rewriter(base_href, html_path, html_glob))
 
 
